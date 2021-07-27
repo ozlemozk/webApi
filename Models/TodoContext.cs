08fbfb8e3b9436a25e6e437c62d52a31bea77c06
Özlem ÖZKAYA <58657181+ozlemozk@users.noreply.github.com>
2021-07-27 17:25:54 +0300

    Add files via upload

---
+++ Models/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapi.Models
+{
+    public class TodoContext : DbContext //*databse crud yapmayı sağlar
+    {
+        public TodoContext(DbContextOptions<TodoContext> options):base (options)
+        { }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
